29ed2b62a7c469a214f1b8334d947390abb6c1bc
jihad mimouni <Jihadmimouni6@gmail.com>
2024-08-21 23:32:30 +0100

    chore: Add second screen and navigation functionality

---
+++ lib/second_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Second Screen!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
